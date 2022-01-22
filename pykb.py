b4cccb1cf33bb638daf0078782b5c0856ee0551d
Daniel Widyono <widyono2@Danielle-Wagners-iMac.local>
2022-01-22 13:25:17 -0500

    fix https://github.com/widyono/pykb/issues/1

--- pykb.py
+++ pykb.py
@@ -27,13 +27,28 @@ FG = (255,255,255)
 # wait this long after any acceptable keypress before continuing with event loop
 # to avoid bounces and spamming
 DELAY_MS = 500
-HYSTERESIS_MS = 50
+HYSTERESIS_MS = 30
 
 # FPS for display update rate, 60 for minimal responsiveness
 FPS = 60
 
 # which key values are accepted for feedback
-allowed_keys = list(range(ord('a'),ord('z')+1)) + list(range(ord('0'),ord('9')+1))
+numpad_keys = list(range(K_KP1,K_KP0 + 1))
+alpha_keys = list(range(ord('a'),ord('z')+1))
+number_keys = list(range(ord('0'),ord('9')+1))
+allowed_keys = alpha_keys + number_keys + numpad_keys
+numpad_to_number = {
+        K_KP1:ord('1'),
+        K_KP2:ord('2'),
+        K_KP3:ord('3'),
+        K_KP4:ord('4'),
+        K_KP5:ord('5'),
+        K_KP6:ord('6'),
+        K_KP7:ord('7'),
+        K_KP8:ord('8'),
+        K_KP9:ord('9'),
+        K_KP0:ord('0'),
+}
 
 # prevent key bounce, spamming
 active_key = None
@@ -51,11 +66,12 @@ while True:
         if event.type == KEYDOWN:
             if event.key in allowed_keys:
                 # if this is a second (or greater) keypress then ignore
-                print(chr(event.key), pygame.time.get_ticks() - active_keypress_time)
                 if active_key:
                     continue
                 if active_keypress_time > 0 and (pygame.time.get_ticks() - active_keypress_time < (DELAY_MS + HYSTERESIS_MS)):
                     continue
+                if event.key in numpad_keys:
+                    event.key = numpad_to_number[event.key]
                 active_key = event.key
                 active_keypress_time = pygame.time.get_ticks()
                 screen.fill(BG)
@@ -65,6 +81,8 @@ while True:
                 pygame.time.wait(DELAY_MS)
 
         if event.type == KEYUP:
+            if event.key in numpad_keys:
+                event.key = numpad_to_number[event.key]
             if event.key != active_key:
                 continue
             active_key = None
