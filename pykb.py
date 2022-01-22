bbfde56ec8a93ebfe251a3f1531466461cb5f5dd
Daniel Widyono <widyono2@Danielle-Wagners-iMac.local>
2022-01-22 09:46:47 -0500

    allow for different display sizes

--- pykb.py
+++ pykb.py
@@ -2,10 +2,16 @@ import pygame, sys
 from pygame.locals import *
 pygame.init()
 display_info = pygame.display.Info()
-screen = pygame.display.set_mode((display_info.current_w,display_info.current_h))
+#print(display_info)
+display_width=display_info.current_w
+display_height=display_info.current_h
+vertical_margin=100
+char_height=int(display_height-(vertical_margin*2))
+horizontal_margin=int((display_width-char_height)/2)
+screen = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption("pykb")
 clock = pygame.time.Clock()
-key_font = pygame.font.SysFont('arial',800)
+key_font = pygame.font.SysFont('arial',char_height)
 BG = (0,0,0)
 FG = (255,255,255)
 allowed_keys = list(range(ord('a'),ord('z'))) + list(range(ord('0'),ord('9')))
@@ -23,7 +29,7 @@ while True:
                 continue
             screen.fill(BG)
             key_display = key_font.render(chr(event.key).upper(), True, FG)
-            screen.blit(key_display, (500,100))
+            screen.blit(key_display, (horizontal_margin,vertical_margin))
             pygame.display.update()
     if event.type == KEYUP:
         #print(event)
