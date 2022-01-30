e435b9d5be5da04be49a12a1c039db559bf9638f
Dan Widyono <widyono@Dans-MacBook-Pro.local>
2022-01-30 13:58:16 -0500

    clean up image management
      auto rotate based on exif header
      scale while retaining original aspect ratio
      search for possible fonts, and allow user specification
      allow user to specify media directory and minimum display duration

--- pykb.py
+++ pykb.py
@@ -2,8 +2,10 @@ import pygame, sys
 from pygame.locals import *
 import os, string, random
 from pathlib import Path
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ExifTags
 from collections import defaultdict
+import argparse
+from pprint import pformat
 
 # where to store/find our images and sounds
 mediadir = Path.home() / "pykb-media"
@@ -30,12 +32,9 @@ FG = (255,255,255)
 IMG_X = 500
 IMG_Y = 500
 
-# font to use for default keycap images
-font = ImageFont.truetype('Monaco.ttf', size=IMG_Y)
-
-# wait this long after any acceptable keypress before continuing with event loop
+# wait this long (in milliseconds) after any acceptable keypress before continuing with event loop
 #  to avoid bounces and spamming
-DELAY_MS = 500
+DURATION_MS = 1500
 HYSTERESIS_MS = 30
 
 # FPS for display update rate, 60 for minimal responsiveness
@@ -67,6 +66,46 @@ active_keypress_time = 0
 
 
 
+parser = argparse.ArgumentParser(description="Keyboard Playground")
+parser.add_argument("-f","--font",help=f"Specify font name")
+parser.add_argument("-m","--mediadir",help=f"Alternate media directory (instead of {mediadir})")
+parser.add_argument("-d","--duration",default=DURATION_MS,help=f"Minimum duration (in milliseconds) to display image")
+parser.add_argument("-D","--debug",action="store_true",default=False,help="Debug")
+args = parser.parse_args()
+
+def dprint(*dprint_args,**dprint_kwargs):
+    if args.debug:
+        print(*dprint_args,**dprint_kwargs)
+
+if args.mediadir:
+    if os.path.isdir(args.mediadir):
+        mediadir=Path(args.mediadir)
+    else:
+        print(f"{args.mediadir} is not a directory.")
+        exit(1)
+
+for EXIFTAG_ORIENTATION in ExifTags.TAGS.keys():
+    if ExifTags.TAGS[EXIFTAG_ORIENTATION]=='Orientation':
+        break
+
+# font to use for default keycap images
+font_preferences = ['Monaco.ttf', 'Keyboard.ttf']
+if args.font:
+    font_preferences.insert(0, args.font)
+found_font = None
+found_font_name = None
+for try_font in font_preferences:
+    try:
+        found_font = ImageFont.truetype(try_font, size=IMG_Y)
+        found_font_name = try_font
+        break
+    except OSError:
+        continue
+if not found_font:
+    print(f"Could not find any of these fonts: {font_preferences}")
+    exit(1)
+dprint(f"Using font: {found_font_name}")
+
 for keycap in all_keycaps:
 
     """
@@ -88,10 +127,10 @@ for keycap in all_keycaps:
     if not os.path.exists(keycap_font_imagefile):
         image = Image.new('RGB', (IMG_X, IMG_Y))
         dl = ImageDraw.Draw(image)
-        (width,height)=font.getsize(keycap_repr)
+        (width,height)=found_font.getsize(keycap_repr)
         xoff=int((IMG_X-width)/2)
         yoff=int((IMG_Y-height)/2)-50
-        dl.text((xoff,yoff), keycap_repr, font=font, fill=FG)
+        dl.text((xoff,yoff), keycap_repr, font=found_font, fill=FG)
         image.save(str(keycap_font_imagefile),"PNG")
 
     for (dirpath, dirnames, filenames) in os.walk(keycap_mediadir):
@@ -115,6 +154,8 @@ for keycap in all_keycaps:
                 continue
             if not media_options[keycap][basename][MEDIA_SND]:
                 media_options[keycap][basename][MEDIA_SND] = media_options[keycap][keycap][MEDIA_SND]
+
+dprint(f"media_options:\n{pformat(media_options)}")
 
 pygame.init()
 pygame.mixer.init()
@@ -150,7 +191,7 @@ while True:
                 # if this is a second (or greater) keypress then ignore
                 if active_key:
                     continue
-                if active_keypress_time > 0 and (pygame.time.get_ticks() - active_keypress_time < (DELAY_MS + HYSTERESIS_MS)):
+                if active_keypress_time > 0 and (pygame.time.get_ticks() - active_keypress_time < (args.duration + HYSTERESIS_MS)):
                     continue
                 if event.key in numpad_keys:
                     event.key = numpad_to_number[event.key]
@@ -160,12 +201,27 @@ while True:
                 keycap_random_basename = random.choice(list(media_options[chr(active_key)].keys()))
                 keycap_media = media_options[chr(active_key)][keycap_random_basename]
                 if keycap_media[MEDIA_IMG]:
-                    screen.blit(pygame.image.load(keycap_media[MEDIA_IMG]), (horizontal_margin,vertical_margin))
+                    exiftags = Image.open(keycap_media[MEDIA_IMG]).getexif()
+                    keycap_image = pygame.image.load(keycap_media[MEDIA_IMG])
+                    if EXIFTAG_ORIENTATION in exiftags:
+                        if exiftags[EXIFTAG_ORIENTATION] == 3:
+                            keycap_image=pygame.transform.rotate(keycap_image, 180)
+                        elif exiftags[EXIFTAG_ORIENTATION] == 6:
+                            keycap_image=pygame.transform.rotate(keycap_image, 270)
+                        elif exiftags[EXIFTAG_ORIENTATION] == 8:
+                            keycap_image=pygame.transform.rotate(keycap_image, 90)
+
+                    (img_x, img_y) = keycap_image.get_size()
+                    img_max = max(img_x, img_y)
+                    img_ratio_x = img_x / img_max
+                    img_ratio_y = img_y / img_max
+                    keycap_image = pygame.transform.scale(keycap_image, (int(IMG_X * img_ratio_x), int(IMG_Y * img_ratio_y)))
+                    screen.blit(keycap_image, (horizontal_margin,vertical_margin))
                 if keycap_media[MEDIA_SND]:
                     pygame.mixer.music.load(keycap_media[MEDIA_SND])
                     pygame.mixer.music.play(1)
                 pygame.display.update()
-                pygame.time.wait(DELAY_MS)
+                pygame.time.wait(args.duration)
 
         if event.type == KEYUP:
             if event.key in numpad_keys:
